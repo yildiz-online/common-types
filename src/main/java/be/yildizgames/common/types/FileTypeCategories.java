92bb839f8ad7f2408aab8f66d6250f2e1c2b9b6b
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2024-07-02 16:05:48 +0200

    [WIP] Support webp.

--- src/main/java/be/yildizgames/common/types/FileTypeCategories.java
+++ src/main/java/be/yildizgames/common/types/FileTypeCategories.java
@@ -1,17 +1,16 @@
 /*
- * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
- *  Copyright (c) 2023 Grégory Van den Borre
- *  More infos available: https://engine.yildiz-games.be
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- *  documentation files (the "Software"), to deal in the Software without restriction, including without limitation
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- *  permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright
- *  notice and this permission notice shall be included in all copies or substantial portions of the  Software.
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- *  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
+ This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ Copyright (c) 2024 Grégory Van den Borre
+ More infos available: https://engine.yildiz-games.be
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright
+ notice and this permission notice shall be included in all copies or substantial portions of the  Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package be.yildizgames.common.types;
@@ -27,8 +26,8 @@ import java.util.Optional;
  */
 public enum FileTypeCategories {
 
-    IMAGES(new FileTypeCategory("images").addType(FileTypes.PNG).addType(FileTypes.JPG).addType(FileTypes.BMP).addType(FileTypes.GIF)),
-    ARCHIVES(new FileTypeCategory("archive").addType(FileTypes.ZIP).addType(FileTypes.SEVEN_ZIP).addType(FileTypes.RAR));
+    IMAGES(new FileTypeCategory().addType(FileTypes.PNG).addType(FileTypes.JPG).addType(FileTypes.BMP).addType(FileTypes.GIF).addType(FileTypes.WEBP)),
+    ARCHIVES(new FileTypeCategory().addType(FileTypes.ZIP).addType(FileTypes.SEVEN_ZIP).addType(FileTypes.RAR));
 
     private final FileTypeCategory category;
 
@@ -37,12 +36,12 @@ public enum FileTypeCategories {
     }
 
     public final Optional<FileType> is(InputStream stream) throws IOException {
-        return category.is(stream);
+        return this.category.is(stream);
     }
 
     public final Optional<FileType> is(Path file) throws IOException {
         try (var is = Files.newInputStream(file)){
-            return category.is(is);
+            return this.category.is(is);
         }
     }
 }
--- src/main/java/be/yildizgames/common/types/FileTypes.java
+++ src/main/java/be/yildizgames/common/types/FileTypes.java
@@ -1,22 +1,27 @@
 /*
- * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
- *  Copyright (c) 2023 Grégory Van den Borre
- *  More infos available: https://engine.yildiz-games.be
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- *  documentation files (the "Software"), to deal in the Software without restriction, including without limitation
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- *  permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright
- *  notice and this permission notice shall be included in all copies or substantial portions of the  Software.
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- *  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
+ This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ Copyright (c) 2024 Grégory Van den Borre
+ More infos available: https://engine.yildiz-games.be
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright
+ notice and this permission notice shall be included in all copies or substantial portions of the  Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package be.yildizgames.common.types;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
+
+import static be.yildizgames.common.types.FileType.TO_IGNORE;
 
 /**
  * @author Grégory Van den Borre
@@ -36,6 +41,8 @@ public enum FileTypes {
     GIF(new FileType("gif", List.of("gif"), new FileType.FileTypeBytes(List.of(
             new int[]{0x47, 0x49, 0x46, 0x38, 0x37, 0x61},
             new int[]{0x47, 0x49, 0x46, 0x38, 0x39, 0x61})))),
+    WEBP(new FileType("webp", List.of("webp"), new FileType.FileTypeBytes(
+            List.of(new int[] {0x52, 0x49, 0x46, 0x46, TO_IGNORE, TO_IGNORE, TO_IGNORE, TO_IGNORE, 0x57, 0x45, 0x42, 0x50, 0x56, 0x50, 0x38})))),
     ZIP(new FileType("zip", List.of("zip"), new FileType.FileTypeBytes(List.of(
             new int[]{0x50, 0x4b, 0x03, 0x04},
             new int[]{0x50, 0x4b, 0x05, 0x06})))),
@@ -54,5 +61,16 @@ public enum FileTypes {
 
     public final FileType getType() {
         return this.type;
+    }
+
+    public static Optional<FileType> is(Path file, FileTypes... expected) throws IOException {
+        try (var is = Files.newInputStream(file)){
+            for(var e : expected) {
+                if(e.type.is(is)) {
+                    return Optional.of(e.type);
+                }
+            }
+            return Optional.empty();
+        }
     }
 }
